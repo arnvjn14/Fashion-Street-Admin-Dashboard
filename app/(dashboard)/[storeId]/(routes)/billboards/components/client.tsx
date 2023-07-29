2bb047289e716379e0945d15135310b18a1c1db1
Arnav Jain <arnavj343@gmail.com>
2023-07-29 18:10:56 +0530

    billboard schema, cloudinary added, billboard creation-form

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Heading } from "@/components/ui/heading";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
+import { Billboard } from "@prisma/client";
+
+interface BillboardClientProps {
+  data: Billboard[];
+}
+
+const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
+};
+
+export default BillboardClient;
---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,21 @@
+import BillboardClient from "./components/client";
+import prismadb from "@/lib/prismadb";
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-8 p-8 pt-6">
+        <BillboardClient data={billboards} />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardsPage;
