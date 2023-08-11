23c2104428a3f63bc4fc220332701625db518c3f
Arnav Jain <arnavj343@gmail.com>
2023-08-11 16:30:10 +0530

    api-list is added on billboard page,one change in middleware

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -7,7 +7,7 @@ export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
   return (
     <div>
       <h2 className="text-3xl font-bold tracking-tight mb-2">{title}</h2>
-      <p className="text-sm text-muted-foreground ml-2">{description}</p>
+      <p className="text-sm text-muted-foreground ml-1">{description}</p>
     </div>
   );
 };
